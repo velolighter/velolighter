b266286424178e96322382bc67f8e3fde249ddda
이한승 <pullthechin@gmail.com>
2021-03-26 14:53:42 +0900

    no message

--- source/Popup/Popup.tsx
+++ source/Popup/Popup.tsx
@@ -1,52 +1,110 @@
-import * as React from 'react';
-import {browser, Tabs} from 'webextension-polyfill-ts';
-
+import React, {useState} from 'react';
 import './styles.scss';
 
-function openWebPage(url: string): Promise<Tabs.Tab> {
-  return browser.tabs.create({url});
-}
+const addUser = (e): void => {
+  e.preventDefault();
+};
+
+const Tab: React.FC = () => {
+  const [state, setState] = useState(0);
+  return (
+    <div className="wrapper">
+      <div className="tab">
+        <button
+          type="button"
+          onClick={(): void => setState(0)}
+          style={
+            state === 0
+              ? {
+                  borderBottom: '2px solid rgb(12, 166, 120)',
+                  color: 'rgb(12, 166, 120)',
+                  fontWeight: '700',
+                }
+              : {borderBottom: 'none'}
+          }
+        >
+          팔로잉 추가
+        </button>
+        <button
+          type="button"
+          onClick={(): void => setState(1)}
+          style={
+            state === 1
+              ? {
+                  borderBottom: '2px solid rgb(12, 166, 120)',
+                  color: 'rgb(12, 166, 120)',
+                  fontWeight: '700',
+                }
+              : {borderBottom: 'none'}
+          }
+        >
+          팔로잉 삭제
+        </button>
+      </div>
+      <div className="content">
+        <form
+          className="add-user"
+          style={state === 0 ? {display: 'block'} : {display: 'none'}}
+        >
+          <input
+            type="text"
+            placeholder="팔로잉할 사용자를 추가해주세요"
+            className="add-user__input"
+          />
+          <button className="add-user__button" type="submit" onClick={addUser}>
+            사용자 추가
+          </button>
+        </form>
+        <div
+          className="delete-user"
+          style={state === 1 ? {display: 'block'} : {display: 'none'}}
+        >
+          <ul>
+            <li>
+              mowinckel
+              <button type="button">삭제</button>
+            </li>
+            <li>
+              openhub
+              <button type="button">삭제</button>
+            </li>
+            <li>
+              juunini
+              <button type="button">삭제</button>
+            </li>
+            <li>
+              white-jang
+              <button type="button">삭제</button>
+            </li>
+            <li>
+              white-jang
+              <button type="button">삭제</button>
+            </li>
+            <li>
+              white-jang
+              <button type="button">삭제</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const CountUsers: React.FC = () => {
+  return (
+    <div className="count-users">
+      팔로우한 사용자 수:<span>1</span>
+    </div>
+  );
+};
 
 const Popup: React.FC = () => {
   return (
-    <section id="popup">
-      <h2>WEB-EXTENSION-STARTER</h2>
-      <button
-        id="options__button"
-        type="button"
-        onClick={(): Promise<Tabs.Tab> => {
-          return openWebPage('options.html');
-        }}
-      >
-        Options Page
-      </button>
-      <div className="links__holder">
-        <ul>
-          <li>
-            <button
-              type="button"
-              onClick={(): Promise<Tabs.Tab> => {
-                return openWebPage(
-                  'https://github.com/velolighter/web-extension-starter'
-                );
-              }}
-            >
-              GitHub
-            </button>
-          </li>
-          <li>
-            <button
-              type="button"
-              onClick={(): Promise<Tabs.Tab> => {
-                return openWebPage('https://www.buymeacoffee.com/velolighter');
-              }}
-            >
-              Buy Me A Coffee
-            </button>
-          </li>
-        </ul>
-      </div>
-    </section>
+    <div className="pop-up">
+      <Tab />
+      <CountUsers />
+    </div>
   );
 };
 
