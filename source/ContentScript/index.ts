82e2a445fcadb273462ce7b7ad901e9a12d844d5
이한승 <pullthechin@gmail.com>
2021-04-17 14:32:32 +0900

    setInterval 함수에서 followers 상태가 최신 상태의 값으로 업데이트가 되지 않습니다.

--- source/ContentScript/index.ts
+++ source/ContentScript/index.ts
@@ -1,29 +1,31 @@
 import {BrowserStorage} from '../libs/storage';
 
-const userInfos = document.getElementsByClassName('userinfo');
+const userInfo = document.getElementsByClassName('userinfo');
 
-BrowserStorage.getStorage().then((storage) => {
-  const {followers} = storage;
-  console.log(followers);
-  function addTag(infos): void {
-    for (let index = 0; index < infos.length; index += 1) {
-      const userName = infos[index].children[1].textContent
-        .replace('by', '')
-        .trim();
-      const span = infos[index].children[1];
-      for (const follower of followers) {
-        if (follower.userName === userName && span.className === '') {
-          span.className = 'following';
-          span.style.cssText =
-            'background: linear-gradient(transparent 80%, rgb(64, 240, 175, 0.8) 80%);';
+setInterval(function callBack() {
+  BrowserStorage.getStorage().then((storage) => {
+    const followers = storage.list();
+    console.log(followers);
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+    function addTag(infos: any): void {
+      for (let index = 0; index < infos.length; index += 1) {
+        const userName = infos[index].children[1].textContent
+          .replace('by', '')
+          .trim();
+
+        const span = infos[index].children[1];
+        for (const follower of followers) {
+          if (follower.userName === userName && span.className === '') {
+            span.className = 'following';
+            span.style.cssText =
+              'background: linear-gradient(transparent 80%, rgb(64, 240, 175, 0.8) 80%);';
+          }
         }
       }
     }
-  }
 
-  setInterval(function callBack() {
-    addTag(userInfos);
-  }, 2000);
-});
+    addTag(userInfo);
+  });
+}, 2000);
 
 export {};
