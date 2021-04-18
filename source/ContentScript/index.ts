d73ca6713091b45d8b9dc4d7f522d81439ced57b
이한승 <pullthechin@gmail.com>
2021-04-18 14:54:24 +0900

    팔로우한 사용자를 삭제하게 되었을때 velog 화면에서 표시하지 않는 기능을 추가하였습니다.

--- source/ContentScript/index.ts
+++ source/ContentScript/index.ts
@@ -2,28 +2,64 @@ import {BrowserStorage} from '../libs/storage';
 
 const userInfos = document.getElementsByClassName('userinfo');
 
-BrowserStorage.getStorage().then((storage) => {
-  const {followers} = storage;
-  console.log(followers);
-  function addTag(infos): void {
-    for (let index = 0; index < infos.length; index += 1) {
-      const userName = infos[index].children[1].textContent
-        .replace('by', '')
-        .trim();
-      const span = infos[index].children[1];
-      for (const follower of followers) {
-        if (follower.userName === userName && span.className === '') {
-          span.className = 'following';
-          span.style.cssText =
-            'background: linear-gradient(transparent 80%, rgb(64, 240, 175, 0.8) 80%);';
+setInterval(function callBack() {
+  BrowserStorage.getStorage().then((storage) => {
+    storage.sync();
+
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+    function highLightBorder(infosHighlight: any): void {
+      for (let index = 0; index < infosHighlight.length; index += 1) {
+        const userName = infosHighlight[index].children[1].textContent
+          .replace('by', '')
+          .trim();
+        const styledDiv = infosHighlight[index].parentElement.parentElement;
+        const span = infosHighlight[index].children[1];
+        const followers = storage.list();
+
+        for (const follower of followers) {
+          if (follower.userName === userName && span.className === '') {
+            span.className = 'following';
+            styledDiv.style.cssText = 'border: 3px solid rgb(18, 184, 134);';
+          }
         }
       }
     }
-  }
 
-  setInterval(function callBack() {
-    addTag(userInfos);
-  }, 2000);
-});
+    function getUserNames(): string[] {
+      const followers = storage.list();
+      const userNamesArray = [];
+
+      for (const follower of followers) {
+        userNamesArray.push(follower.userName);
+      }
+
+      return userNamesArray;
+    }
+
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+    function deleteBorder(infosDelete: any): void {
+      const userNamesArray = getUserNames();
+
+      for (let index = 0; index < infosDelete.length; index += 1) {
+        const userName = infosDelete[index].children[1].textContent
+          .replace('by', '')
+          .trim();
+        const styledDiv = infosDelete[index].parentElement.parentElement;
+        const span = infosDelete[index].children[1];
+
+        if (
+          !userNamesArray.includes(userName) &&
+          span.className === 'following'
+        ) {
+          span.className = '';
+          styledDiv.style.cssText = 'border: none;';
+        }
+      }
+    }
+
+    highLightBorder(userInfos);
+    deleteBorder(userInfos);
+  });
+}, 100);
 
 export {};
